b36aab067fe1d09e6c7cdbba6efc622907b29d17
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2019-12-09 06:42:12 -0500

    null and split functions

--- lambdata-KhaSab5292/df_utils.py
+++ lambdata-KhaSab5292/df_utils.py
@@ -4,8 +4,19 @@ utililty functions for working with Dataframes
 
 import pandas 
 import numpy
+from sklearn.model_selection import train_test_split
 
 TEST_DF = pandas.DataFrame([1,2,3,4,5])
 
 def null_check(df):
-    return df.isnull().sum()
+    null_table = df.isnull().sum()
+    return null_table
+    
+def train_val_test(df, train_size, val_size, test_size):
+    assert train_size + val_size + test_size == 1 
+    
+    train, test = train_test_split(df, train_size = train_size + val_size, test_size = test_size,
+			           random_state = 42)
+    train, val = train_test_split(train, train_size = train_size, test_size = val_size,
+			          random_state = 42)
+    return train, val, test
