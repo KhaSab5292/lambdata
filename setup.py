a74b7ac9f4be1c96792d68c231f5d26926fc6455
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-01-13 13:57:43 -0500

    updates

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata-KhaSab5292",
+    name="lambdata-ds10",
     version = "0.1.1",
     author = "KhaSab5292",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://lambdaschool.com/courses/data-science",
+    url="https://github.com/KhaSab5292/lambdata-ds10",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires = REQUIRED,
