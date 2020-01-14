1e32812c70e19fc1e1af76c195bcdabec3a43256
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-01-13 23:18:29 -0500

    added new function

--- lambdata_KhaSab5292/df_utils.py
+++ lambdata_KhaSab5292/df_utils.py
@@ -10,13 +10,19 @@ TEST_DF = pd.DataFrame([1, 2, 3, 4, 5])
 
 
 def null_check(df):
-    """ Checks a dataframe for nulls."""
+    """Checks a dataframe for nulls."""
     null_table = df.isnull().sum()
     return null_table
 
+def list_to_df(df, the_list, name):
+    """Turns a list into a series and then adds it under a given 
+    name to a given dataframe"""
+    new_column = pd.Series(the_list)
+    df[name] = new_column
+    return df
 
 def find_seconds(d, h, m, s):
-   """ Takes in integer number of days, hours, minutes, and seconds. 
-   Returns total number of seconds."""
-   seconds = 86400*d + 3600*h + 60*m + s
-   return seconds
+    """Takes in integer number of days, hours, minutes, and seconds. 
+    Returns total number of seconds."""
+    seconds = 86400*d + 3600*h + 60*m + s
+    return seconds
