ba67779ff4a8f2d59284f1d9a2e5e1faef3a2bce
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-01-17 06:39:15 -0500

    pep8

--- lambdata_KhaSab5292/test.py
+++ lambdata_KhaSab5292/test.py
@@ -1,12 +1,15 @@
 import unittest
 from df_utils import find_seconds
 
+
 class Seconds_test(unittest.TestCase):
     """Obligatory docstring test root function!"""
     def test_minute(self):
         self.assertEqual(find_seconds(0, 0, 1, 0), 60)
+
     def test_hour(self):
         self.assertEqual(find_seconds(0, 1, 0, 0), 3600)
+
     def test_ones(self):
         self.assertEqual(find_seconds(1, 1, 1, 1), 90061)
 
